4335fb489fca8cd2f11d310263a25798c431c021
Teodor <tosheca@icloud.com>
2020-03-06 11:05:15 +0200

    Register Screen.

---
+++ AegiS/RegisterViewController.swift
@@ -0,0 +1,60 @@
+//
+//  RegisterViewController.swift
+//  AegiS
+//
+//  Created by Teodor Pavlov on 3/6/20.
+//  Copyright © 2020 Teodor Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var registerView: UIView!
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var surnameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    @IBOutlet weak var registerButton: UIButton!
+    
+    @IBOutlet weak var backToLoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        registerView.clipsToBounds = true
+        registerView.layer.cornerRadius = 10
+        registerView.backgroundColor = UIColor.white.withAlphaComponent(0.98)
+        
+        registerButton.clipsToBounds = true
+        registerButton.layer.cornerRadius = 10
+        
+        backToLoginButton.clipsToBounds = true
+        backToLoginButton.layer.cornerRadius = 10
+    }
+    
+    @IBAction func register(_ sender: Any) {
+        
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
